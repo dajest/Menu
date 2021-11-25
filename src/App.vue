6c3e5c74b1daa389fce73c49bf927b5e42a7e715
Dmitrij Panasiuk <dmitrij.panasiuk.code@gmail.com>
2021-11-25 15:38:22 +0300

    1-st Prototype

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,182 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+
+  <div class="wrap">
+    <div class="week-list">
+
+      <div class="single-week-area" v-for="singleWeek in weeks" :key="singleWeek.weekNumber">
+        <h1>Неделя - {{singleWeek.weekNumber}}</h1>
+        <draggable
+          class="dragArea list-group"
+          :list="singleWeek.dishesAdded"
+          group="dishes"
+          @change="log"
+        >
+        <div
+          class="list-group-item"
+          v-for="(dish, idx) in singleWeek.dishesAdded"
+          :key="idx"
+        >
+          {{ dish.name }}
+        </div>
+        </draggable>
+      </div>
+
+
     </div>
-    <router-view/>
+    <div class="dish-list">
+      <draggable
+        class="dragArea list-group"
+        :list="list2"
+        :group="{ name: 'dishes', pull: 'clone', put: false }"
+        @change="log"
+      >
+        <div
+          class="list-group-item"
+          v-for="element in list2"
+          :key="element.name"
+        >
+          {{ element.name }}
+        </div>
+      </draggable>
+    </div>
   </div>
+
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import draggable from "vuedraggable";
+export default {
+  name: "clone",
+  display: "Clone",
+  order: 2,
+  components: {
+    draggable
+  },
+  data() {
+    return {
+      weeks: [
+        {
+          weekNumber: 1,
+          dishesAdded: []
+        },
+        {
+          weekNumber: 2,
+          dishesAdded: []
+        },
+        {
+          weekNumber: 3,
+          dishesAdded: []
+        }, 
+        {
+          weekNumber: 4,
+          dishesAdded: []
+        }, 
+        {
+          weekNumber: 5,
+          dishesAdded: []
+        }, 
+        {
+          weekNumber: 6,
+          dishesAdded: []
+        }, 
+        {
+          weekNumber: 7,
+          dishesAdded: []
+        }, 
+        {
+          weekNumber: 8,
+          dishesAdded: []
+        }      
+      ],
+      list1: [],
+      list2: [
+        { name: "Борщ", id: 1 },
+        { name: "Рассольник", id: 2 },
+        { name: "Щавелёвый", id: 3 },
+        { name: "Гречка", id: 4 },
+        { name: "Рис", id: 5 },
+        { name: "Паста", id: 6 },
+        { name: "Свинина", id: 7 },
+        { name: "Говядина", id: 8 },
+        { name: "Курица", id: 9 },
+        { name: "Рыба", id: 10 },
+      ]
+    };
+  },
+  methods: {
+    log: function(evt) {
+      window.console.log(evt);
     }
   }
+};
+</script>
+<style lang="scss">
+
+.wrap {
+  display: grid;
+  grid-template-columns: 4fr 1fr;
+  grid-column-gap: 20px;
+}
+
+.week-list, .dish-list {
+  box-sizing: border-box;
+  border: 1px solid darkcyan;
+  padding: 20px;
+  height: 100%;
+}
+
+.week-list {
+  display: grid;
+  grid-template-columns: repeat(4,1fr);
+  grid-gap: 10px;
+  
+  .single-week-area {
+    box-sizing: border-box;
+    border: 1px solid darkgray;
+    min-height: 200px;
+    display: flex;
+    flex-direction: column;
+    // box-shadow: 1px 0px 26px -1px rgba(34, 60, 80, 0.21);
+
+    h1 {
+      font-size: 20px;
+      text-align: center;
+      margin: 0;
+      border-bottom: 1px solid darkgray;
+    }
+    
+    .dragArea.list-group {
+      box-sizing: border-box;
+      padding: 20px;
+      // background-color: darkred;
+      flex: 1;
+    }
+
+    .list-group-item {
+      margin-bottom: 5px;
+
+      &:last-child {
+        margin: 0;
+      }
+    }
+  }
+  
+}
+
+
+
+.list-group-item {
+    position: relative;
+    display: block;
+    padding: 0.75rem 1.25rem;
+    margin-bottom: -1px;
+    background-color: #fff;
+    border: 1px solid rgba(0,0,0,.125);
+  cursor: move;
+}
+
+.list-group-item:first-child {
+    border-top-left-radius: 0.25rem;
+    border-top-right-radius: 0.25rem;
 }
 </style>
